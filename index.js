c124509b69da6e293475d76a205219399e5cbdde
David Sanchez <cristiansanchezh@gmail.com>
2021-12-02 21:57:28 -0500

    se modifico la url para que apunte a la nube

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const mongoose= require('mongoose');
 const app = require('./app');
 const port = 3000;
+// const conexionLocal = 'mongodb://localhost:27017/tiquetfast';
+//const conexionNube ='mongo "mongodb+srv://cluster0.uyltm.mongodb.net/myFirstDatabase" --username tiquetFast';
+//const uri = "mongodb+srv://my_login_id:TFDBaz9KdWJtBqA@cluster0.uyltm.mongodb.net/tiquetFast?retryWrites=true&w=majority";
+//const uri = 'mongodb+srv://tiquetfast:<TFDBaz9KdWJtBqA>@cluster0.uyltm.mongodb.net/test';
+//mongodb+srv://tiquetFast:<TFDBaz9KdWJtBqA>@cluster0.uyltm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+const uri = 'mongodb+srv://tiquetFast:0000@cluster0.uyltm.mongodb.net/test';
 
-mongoose.connect('mongodb://localhost:27017/tiquetfast', (err,res)=>{
+mongoose.connect(uri, (err,res)=>{
     if(err){
         console.log(`El error es ${err}`);
     }else{
